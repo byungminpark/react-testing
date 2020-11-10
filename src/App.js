32c9bcf0563754368fac86d0e661a69671018c94
ByungminPark <qrqudals777@gmail.com>
2020-11-10 22:14:56 +0900

    Test example 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState, useEffect } from 'react'; 
+
+// import { screen } from '@testing-library/react';
+// console.log(screen.queryByText('signed in as', {}));
+// console.log(screen.findByText(/^signed in as$/i));
+
+const getUser = () => Promise.resolve({ id: '1', name: 'Robin' });
+ 
 function App() {
+  const [search, setSearch] = useState('');
+  const [user, setUser] = useState(null);
+ 
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
+ 
+  const handleChange = (event) => setSearch(event.target.value);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <p>Signed in as {user.name}</p> : null}
+ 
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+ 
+      <p>Searches for {search ? search : '...'}</p>
     </div>
   );
 }
 
+
+export function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+} 
+
+
 export default App;
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App, { Search } from './App';
+
+describe('App', () => {
+  // 1
+  it('waits element rendered by fetching effect', async () => {
+    render(<App />);
+    // Implicit test
+    await screen.findByText(/Signed in as/);
+  });
+  
+  // 2
+  it('changes element\'s text after type event firing', () => {
+    render(<App />);
+    // Before
+    expect(screen.queryByText(/Searches for Song/)).toBeNull();
+    
+    // Firing
+    userEvent.type(screen.getByRole('textbox'), 'Song');
+    
+    // After
+    expect(screen.getByText(/Searches for Song/)).toBeInTheDocument();
+  });
+  
+  // 3
+  it('calls the onChange callback handler for Search', () => {
+    const onChange = jest.fn(); // mock
+
+    render(
+      <Search value="" onChange={onChange}>
+        Search:
+      </Search>
+    );
+
+    userEvent.type(screen.getByRole('textbox'), 'Song');
+
+    expect(onChange).toHaveBeenCalledTimes(4);
+  });
 });
